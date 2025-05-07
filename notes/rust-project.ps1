885198de4e3e9c14fd3a90149c3171f4a593a0c1
Will Flores <waflores956@gmail.com>
2025-05-06 21:33:42 -0400

    Remove the helper script for workspace detection.

--- notes/rust-project.ps1
+++
@@ -1,6 +0,0 @@
-& bazelisk run @rules_rust//tools/rust_analyzer:discover_bazel_rust_project # -- --bazel_arg=--watchfs 
-
-write-host "There are a total of $($args.count) arguments"
-for ( $i = 0; $i -lt $args.count; $i++ ) {
-    write-host "Argument  $i is $($args[$i])"
-}
